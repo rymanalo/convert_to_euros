d0a5e08cda09db5a68af1d74676bab2ccc33c48d
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-08 08:58:25 -0700

    Fix rounding.

--- convert_to_euros.rb
+++ convert_to_euros.rb
@@ -6,7 +6,25 @@ def convert_to_euros(num)
   result = JSON.load(file.read)
 
   num * result["rate"]
+
+  str = ""
+  num2 = num.round(2).to_s.split("")
+
+  str << num2.pop
+  str << num2.pop
+  str << num2.pop
+
+  while num2.count > 3
+      str << num2.pop
+      str << num2.pop
+      str << num2.pop + ","
+  end
+  str << num2.join("").reverse << "$"
+  str.reverse
+
 end
+
 
 puts convert_to_euros(1)
 puts convert_to_euros(25)
+puts convert_to_euros(232354.23235)
